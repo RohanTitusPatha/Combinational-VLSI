d5950ccf2b07cac43ffafa20b1f49c14c7a2f7de
Rohan Titus <145809181+RohanTitusPatha@users.noreply.github.com>
2026-09-17 15:39:33 +0530

    Add 4-bit Ripple Carry Adder and testbench

---
+++ RCA_tb.v
@@ -0,0 +1,41 @@
+module RCA_tb;
+
+    reg [3:0] a;
+    reg [3:0] b;
+    reg cin;
+
+    wire [3:0] sum;
+    wire cout;
+
+    RCA dut (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .sum(sum),
+        .cout(cout)
+    );
+
+    initial begin
+
+        $display(" a     b    cin | cout  sum");
+
+        a = 4'b0000; b = 4'b0000; cin = 0;
+        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+
+        a = 4'b0011; b = 4'b0010; cin = 0;
+        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+
+        a = 4'b0101; b = 4'b0011; cin = 0;
+        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+
+        a = 4'b1111; b = 4'b0001; cin = 0;
+        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+
+        a = 4'b1111; b = 4'b1111; cin = 1;
+        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+
+        $finish;
+
+    end
+
+endmodule
