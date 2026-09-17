748c1093be1d493573bb7a064015ba575c52ffe3
Rohan Titus <145809181+RohanTitusPatha@users.noreply.github.com>
2026-09-17 17:49:56 +0530

    Convert RCA to parameterized N-bit design

--- RCA_tb.v
+++ RCA_tb.v
@@ -1,13 +1,15 @@
 module RCA_tb;
 
-    reg [3:0] a;
-    reg [3:0] b;
-    reg cin;
+    parameter N = 4;
 
-    wire [3:0] sum;
+    reg  [N-1:0] a;
+    reg  [N-1:0] b;
+    reg  cin;
+
+    wire [N-1:0] sum;
     wire cout;
 
-    RCA dut (
+    RCA #(.N(N)) dut (
         .a(a),
         .b(b),
         .cin(cin),
@@ -17,22 +19,27 @@ module RCA_tb;
 
     initial begin
 
-        $display(" a     b    cin | cout  sum");
+        $display("   A      B     Cin | Cout   Sum");
 
         a = 4'b0000; b = 4'b0000; cin = 0;
-        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+        #10 $display("%b   %b    %b  |   %b    %b",
+                     a, b, cin, cout, sum);
 
         a = 4'b0011; b = 4'b0010; cin = 0;
-        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+        #10 $display("%b   %b    %b  |   %b    %b",
+                     a, b, cin, cout, sum);
 
         a = 4'b0101; b = 4'b0011; cin = 0;
-        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+        #10 $display("%b   %b    %b  |   %b    %b",
+                     a, b, cin, cout, sum);
 
         a = 4'b1111; b = 4'b0001; cin = 0;
-        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+        #10 $display("%b   %b    %b  |   %b    %b",
+                     a, b, cin, cout, sum);
 
         a = 4'b1111; b = 4'b1111; cin = 1;
-        #10 $display("%b  %b   %b  |  %b   %b", a,b,cin,cout,sum);
+        #10 $display("%b   %b    %b  |   %b    %b",
+                     a, b, cin, cout, sum);
 
         $finish;
 
